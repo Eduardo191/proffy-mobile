e2d67e50a0d4067bc6685e28c1718a8c8e079cfd
Eduardo <feduardo191@gmail.com>
2020-08-14 10:28:48 -0300

    Filtros de professores

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, ScrollView } from 'react-native'
+import { View, ScrollView, Text, TextInput } from 'react-native'
 import styles from './styles'
 
 import PageHeader from '../../components/PageHeader'
@@ -8,7 +8,36 @@ import TeacherItem from '../../components/TeacherItem'
 function TeacherList() {
   return(
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            placeholderTextColor="#c1bcc"
+            style={styles.input}
+            placeholder="Qual a matéria?" 
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                placeholderTextColor="#c1bcc"
+                style={styles.input}
+                placeholder="Qual o dia?" 
+              />  
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                placeholderTextColor="#c1bcc"
+                style={styles.input}
+                placeholder="Qual o horário?" 
+              />  
+            </View>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
